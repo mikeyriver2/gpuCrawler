e52eab413a3d6fff36d8bcbbd6b6f78d5d0d2a0f
mikeyShopeeRiver <93173373+mikeyShopeeRiver@users.noreply.github.com>
2022-01-14 21:53:25 +0800

    Remove Scrappy use Beautifulsoup

--- gpuApp/views.py
+++ gpuApp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Listing
+from bs4 import BeautifulSoup
+import requests
 
 def index(request):
-  return HttpResponse('lol')
+  page = requests.get("https://tipidpc.com/catalog.php?cat=4&sec=s&page=1")
+  soup = BeautifulSoup(page.content, 'html.parser')
+  listItems = soup.select('#item-search-results > li table a')
+
+  for item in listItems:
+    print(item.get_text())
+    print(item['href'])
+
+  nextButton = soup.select(".pager input[value='Next']")
+  return HttpResponse(nextButton)
 
 def create(request):
   listing = Listing.objects.create(
